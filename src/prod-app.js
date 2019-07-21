09352acb68331300500b3af22d5f263fb75b29f6
levvsha <levsha.mikhail@gmail.com>
2019-07-21 13:24:46 +0200

    rework consort line

--- src/prod-app.js
+++ src/prod-app.js
@@ -4,7 +4,7 @@ import * as d3 from 'd3';
 import data from './data.json';
 
 d3.json('https://api.jsonbin.io/b/5d2323ecb7cb986712875151/latest').then(function(text) {
-  if (document.location.pathname === '/row') {
+  if (document.location.search === '?raw') {
     new drawFunction(data);
   } else {
     new drawFunction(text);
